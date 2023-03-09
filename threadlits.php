1bbefbcd475a56847ac81a6e7765f63d290ac2a5
KumarAmrendram <amrendramkumar@gmail.com>
2023-03-09 10:11:58 +0530

    -adds thread lists

---
+++ threadlits.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="shortcut icon" type="image/x-icon" href="/public/favicon.ico">
+
+    <link rel="stylesheet" href="style.css">
+
+
+    <title>Ask Shivaji</title>
+</head>
+
+<body>
+    <?php include "Partials/_header.php" ?>
+
+
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">Welcome to C++ Thread</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <hr class="my-4">
+            <h1 class="display-6">Forum Rules</h1>
+            <p>This forum is for sharing knowledge with each other.</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </p>
+        </div>
+
+
+        <!-- Questions -->
+    </div>
+
+    <div class="container">
+        <h1>Browse Questions</h1>
+        <hr class="my-4">
+        <div class="media my-3 ms-3">
+            <div class="user-img">
+                <div class="user-img">
+                    <img src="img/user.png" alt="...">
+                </div>
+            </div>
+            <div class="media-body">
+                <h5 class="mt-0">unable to install feelings in her.</h5>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to </p>
+            </div>
+        </div>
+
+        <div class="media my-3 ms-3">
+            <div class="user-img">
+                <img src="img/user.png" alt="...">
+            </div>
+            <div class="media-body">
+                <h5 class="mt-0">unable to install feelings in her.</h5>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to </p>
+            </div>
+        </div>
+
+        <div class="media my-3 ms-3">
+            <div class="user-img">
+                <img src="img/user.png" alt="...">
+            </div>
+            <div class="media-body">
+                <h5 class="mt-0">unable to install feelings in her.</h5>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to </p>
+            </div>
+        </div>
+
+        <div class="media my-3 ms-3">
+            <div class="user-img">
+                <img src="img/user.png" alt="...">
+            </div>
+            <div class="media-body">
+                <h5 class="mt-0">unable to install feelings in her.</h5>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to </p>
+            </div>
+        </div>
+
+        <div class="media my-3 ms-3">
+            <div class="user-img">
+                <img src="img/user.png" alt="...">
+            </div>
+            <div class="media-body">
+                <h5 class="mt-0">unable to install feelings in her.</h5>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to </p>
+            </div>
+        </div>
+
+    </div>
+
+    <?php include "Partials/_footer.php" ?>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
