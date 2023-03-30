ba1f411adf6e75b06622e18b9b0ab626e046490a
KumarAmrendram <amrendramkumar@gmail.com>
2023-03-30 20:13:51 +0530

    -rb

--- Partials/_dbconnect.php
+++ Partials/_dbconnect.php
@@ -1,12 +1,16 @@
 <?php 
 //script to connect to the database
-
-$servername = "localhost";
+session_start();
+session_regenerate_id(true);
+$serverName = "localhost";
 $username = "root";
 $password = "";
 $database = "discussionforum";
+$conn = mysqli_connect($serverName, $username, $password, $database);
 
-
-$conn = mysqli_connect($servername, $username, $password, $database);
-
+// CHECK DATABASE CONNECTION
+if(mysqli_connect_errno()){
+    echo "Connection Failed".mysqli_connect_error();
+    exit;
+}
 ?>
